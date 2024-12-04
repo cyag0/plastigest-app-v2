89165c729dd3e33dd672aac43fdbddf911392e92
Gabito <pv210110238@vallarta.tecmm.edu.mx>
2024-12-03 22:44:44 -0600

    usuario

--- app/(tabs)/(actividades)/(users)/index.tsx
+++ app/(tabs)/(actividades)/(users)/index.tsx
@@ -2,7 +2,7 @@ import { Image, StyleSheet, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import ProTable from "@/components/ProComponents/ProTable";
 import Api from "@/services";
-import { TextInput, Text, Switch } from "react-native-paper";
+import { TextInput, Text, Switch, Checkbox } from "react-native-paper";
 import FormInput from "@/components/FormComponents/FormInput";
 import { Colors } from "@/constants/Colors";
 import Select from "@/components/FormComponents/Select";
@@ -38,16 +38,15 @@ const index = () => {
       resource="users"
       validationScheme={(yup) => {
         return {
-          name: yup.string().required("El nombre es requerido"),
+          /* name: yup.string().required("El nombre es requerido"),
           last_name: yup.string().required("El Apellido es requerido"),
           password: yup.string().required("La contraseña es requerida"),
           email: yup.string().required("El correo es requerido"),
           phone_number: yup.string().required("El nombre es requerido"),
           role_id: yup.number().required("El role es requerido"),
-          location_id:yup.string().required("La sucursal es requerida"),
-          is_active: yup.boolean().required("El estado es requerido")
+          location_id: yup.string().required("La sucursal es requerida"),
+          is_active: yup.boolean().required("El estado es requerido"), */
         };
-
       }}
       columns={[
         {
@@ -63,7 +62,7 @@ const index = () => {
           title: "Contraseña",
           field: "password",
         },
-        
+
         {
           title: "Email",
           field: "email",
@@ -78,13 +77,12 @@ const index = () => {
         },
 
         {
-          title:"Sucusal",
+          title: "Sucusal",
           field: "location_id",
         },
         {
-          title:"Estado",
-          field:"is_active",
-
+          title: "Estado",
+          field: "is_active",
         },
       ]}
       title="Usuarios"
@@ -156,7 +154,7 @@ const index = () => {
                   label={"Numero de telefono"}
                 />
               </View>
-            </View> 
+            </View>
 
             <Select
               placeholder={{
@@ -171,7 +169,7 @@ const index = () => {
               }))}
             />
 
-            <Select
+            {/* <Select
               placeholder={{
                 label: "Selecciona una ubicacion",
                 value: null,
@@ -182,11 +180,35 @@ const index = () => {
                 label: location.name,
                 value: location.id,
               }))}
-            />
-            <Switch value={props.values.status} onValueChange={(value)=>{
-              console.log(value);
+            /> */}
+            {locations.map((location) => (
+              <Checkbox.Item
+                label={location.name}
+                value={location.id}
+                onPress={() => {
+                  props.setFieldValue("locations", {
+                    ...props.values.locations,
+                    [location.id]: !props.values.locations?.[location.id],
+                  });
 
-            }} />;
+                  console.log(props.values.locations);
+                }}
+                status={
+                  props.values.locations?.[location.id]
+                    ? "checked"
+                    : "unchecked"
+                }
+              />
+            ))}
+            <View>
+              <Text>Estado</Text>
+              <Switch
+                value={props.values.is_active}
+                onValueChange={(value) => {
+                  props.setFieldValue("is_active", value);
+                }}
+              />
+            </View>
           </>
         );
       }}
